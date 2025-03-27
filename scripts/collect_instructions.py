6e58466f3c52968839b74bcc7d60d969190c3adb
Márton Kardos <power.up1163@gmail.com>
2025-03-27 13:01:53 +0100

    Fixed instruction generation script

--- scripts/collect_instructions.py
+++ scripts/collect_instructions.py
@@ -4,10 +4,24 @@ import os
 import random
 from pathlib import Path
 
+import mteb
 import openai
 from tqdm import tqdm
 
-from utils import load_tasks
+
+def load_tasks() -> list[mteb.AbsTask]:
+    all_tasks = mteb.get_tasks()
+    all_task_names = [task.metadata.name for task in all_tasks]
+    benchmarks = mteb.get_benchmarks()
+    benchmark_tasks = []
+    for benchmark in benchmarks:
+        benchmark_tasks.extend([task.metadata.name for task in benchmark.tasks])
+    tasks_not_in_benchmarks = set(all_task_names) - set(benchmark_tasks)
+    # This one's biiiig
+    tasks_not_in_benchmarks -= set(["MSMARCOv2"])
+    tasks = mteb.get_tasks(tasks=tasks_not_in_benchmarks)
+    return tasks
+
 
 example_instructions = {
     "STS": ["Retrieve semantically similar text."],
--- scripts/eval_mteb.py
+++ scripts/eval_mteb.py
@@ -1,15 +1,16 @@
+from pathlib import Path
+
 import mteb
 import numpy as np
-from keras.saving import load_model
 from mteb.encoder_interface import Encoder, PromptType
-from sentence_transformers import SentenceTransformer
+
+from post_instruct.model import PostInstruct
 
 
-class PostInstruct(Encoder):
-    def __init__(self):
+class PostInstructWrapper(Encoder):
+    def __init__(self, model_path: str | Path):
         super().__init__()
-        self.model = SentenceTransformer("intfloat/multilingual-e5-large-instruct")
-        self.adaptor = load_model("auto_model/best_model.keras")
+        self.model = PostInstruct(model_path)
 
     def encode(
         self,
@@ -20,24 +21,13 @@ class PostInstruct(Encoder):
         **kwargs,
     ) -> np.ndarray:
         prompt = mteb.get_task(task_name).metadata.prompt
-        if prompt_type == "query":
-            query_embeddings = self.model.encode(
-                [f"Query: {sentence}" for sentence in sentences]
-            )
-            instruction_embedding = self.model.encode([f"Instruction: {prompt}"])
-            instruction_embedding = np.broadcast_to(
-                instruction_embedding,
-                (query_embeddings.shape[0], instruction_embedding.shape[1]),
-            )
-            embeddings = self.adaptor.predict(
-                np.concatenate((instruction_embedding, query_embeddings), axis=1)
-            )
-            embeddings = np.array(embeddings)
-        else:
-            embeddings = self.model.encode(sentences)
+        embeddings = self.model.encode(sentences, instruction=prompt)
         return embeddings
 
 
-task = mteb.get_task("SwednClusteringS2S")
+task = mteb.get_task("ArXivHierarchicalClusteringS2S")
 evaluation = mteb.MTEB(tasks=[task])
-evaluation.run(PostInstruct(), output_folder="results")
+evaluation.run(
+    PostInstructWrapper("models/all-MiniLM-L6-v2-post-instruct/checkpoint-675"),
+    output_folder="results",
+)
